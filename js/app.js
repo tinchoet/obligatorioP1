b9559eea64e718429af60d1aff970bed1ed70112
VascoEt <arieletchebarne@outlook.com>
2024-05-21 22:26:12 -0300

    Arrays

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-window.addEventListener("load", register);
+window.addEventListener("load", init);
 
-function register() {
+function init() { //CAMBIE EL NOMBRE DE LA FUNCION PARA METER TODOS LOS BOTONES ACA Y OTRAS FUNCIONES QUE TENGAMOS QUE CARGAR ENSEGUIDA QUE CARGUE LA PAGINA
+
   document.querySelector("#hideLogin").addEventListener("click", showRegister); // función para esconder login al hacer click en 'registrarse'
   document.querySelector("#register-button").addEventListener("click", registerFunction); // función para validar registro
   document.querySelector("#arrow-container").addEventListener("click", showLogin); // función para mostrar login al clickear en la flecha para ir hacia atrás en el registro
 }
+
+//Listas que necesitamos para almacenar los datos de los objetos.
+//Agregar mas a medida que las necesitemos.
+const arrayAdmins = [{
+  username: 'admin',
+  password: 'admin'
+}];
+
+const arrayProducts = [];
+let arrayUsers = [];
+let arrayShopping = [];
 
 function showRegister() {
   document.querySelector("#login-container").style.display = "none";
@@ -13,6 +25,7 @@ function showRegister() {
   document.querySelector("#logo-container").style.display = "block"; // logo de la tienda
 }
 
+// Mostrar el LogIn al hacer click en la flecha para ir hacia atrás en el registro
 function showLogin() {
   document.querySelector("#login-container").style.display = "block";
   document.querySelector("#register-container").style.display = "none";
@@ -20,6 +33,8 @@ function showLogin() {
   document.querySelector("#logo-container").style.display = "none"; // logo de la tienda
 }
 
+
+// Funcion para validar el registro del usuario
 function registerFunction() {
   let firstName = document.querySelector("#register-name").value;
   let lastName = document.querySelector("#register-surname").value;
@@ -39,10 +54,10 @@ function registerFunction() {
   for (let i = 0; i < password.length; i++) {
     if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) {
       passwordUppercaseCount++;
-    } 
+    }
     if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
       passwordLowercaseCount++;
-    } 
+    }
     if (!isNaN(password.charAt(i))) {
       passwordNumberCount++;
     }
@@ -56,15 +71,15 @@ function registerFunction() {
   ) {
     if (password.length < 5) {
       errorMessage +=
-        "La contraseña debe contener al menos 5 caracteres. <br />";
+        "La contraseña debe contener al menos 5 caracteres. <br>";
     }
     if (passwordUppercaseCount === 0) {
       errorMessage +=
-        "La contraseña debe contener al menos una mayúscula. <br />";
+        "La contraseña debe contener al menos una mayúscula. <br>";
     }
     if (passwordLowercaseCount === 0) {
       errorMessage +=
-        "La contraseña debe contener al menos una minúscula. <br />";
+        "La contraseña debe contener al menos una minúscula. <br>";
     }
     if (passwordNumberCount === 0) {
       errorMessage += "La contraseña debe contener al menos un número.";
@@ -74,6 +89,9 @@ function registerFunction() {
     successMessage = `El registro fue un exito. Tu contraseña es: ${password}`
   }
 
+  //verificación del nombre
+
+  
   document.querySelector("#register-errorMessage").innerHTML = errorMessage;
   document.querySelector("#register-success").innerHTML = successMessage;
 }
