8f107e1368b066400b31e655ebda6853b6a77459
martinleib <m@martinleib.uy>
2024-06-03 00:32:30 -0300

    verificaciones rotas NO ANDA NADA

--- js/app.js
+++ js/app.js
@@ -18,7 +18,6 @@ function showRegister() {
   document.querySelector("#logo-container").style.display = "block"; // logo de la tienda
 }
 
-// Mostrar el LogIn al hacer click en la flecha para ir hacia atrás en el registro
 function showLogin() {
   document.querySelector("#login-container").style.display = "block";
   document.querySelector("#register-container").style.display = "none";
@@ -28,7 +27,6 @@ function showLogin() {
 
 let mainApp = new App();
 
-// Funcion para validar el registro del usuario
 function registerFunction() {
   let firstName = document.querySelector("#register-name").value;
   let lastName = document.querySelector("#register-surname").value;
@@ -36,19 +34,20 @@ function registerFunction() {
   let password = document.querySelector("#register-password").value;
   let creditCard = document.querySelector("#register-card").value;
   let cvc = Number(document.querySelector("#register-cvc").value);
-
-  validateName(firstName, lastName);
-  validateUsername(username);
-  validatePassword(password);
-  validateCard(creditCard);
-  checkVoidInputs(firstName, lastName, username, password, creditCard, cvc);
   
-  if (checkVoidInputs === true) {
-    alert("Completa todos los campos");
-  }
+  document.querySelector("#register-error").innerHTML = "";
 
-  if (nameValidated === true && passwordValidated === true && cardValidated === true && usernameValidated === true && checkVoidInputs === false) {
+  if (
+    validateName(firstName, lastName) === true &&
+    validateUsername(username) === true &&
+    validatePassword(password) === true &&
+    validateCard(creditCard) === true &&
+    validateCVC(cvc) === true &&
+    checkVoidInputs === false
+  ) {
     createNewUser(firstName, lastName, password, username, creditCard, cvc);
+    document.querySelector("#register-success").innerHTML =
+      "Usuario registrado con éxito";
   }
 }
 
@@ -59,7 +58,8 @@ function validateName(firstName, lastName) {
     firstName.charAt(0) === firstName.charAt(0).toLowerCase() ||
     lastName.charAt(0) === lastName.charAt(0).toLowerCase()
   ) {
-    errorMessage = "El Nombre y Apellido deben comenzar en mayúsculas <br>";
+    document.querySelector("#register-error").innerHTML =
+      "El Nombre y Apellido deben comenzar en mayúsculas <br>";
   } else {
     nameValidated = true;
   }
@@ -92,27 +92,25 @@ function validatePassword(password) {
     passwordNumberCount === 0
   ) {
     if (password.length < 5) {
-      errorMessage += "La contraseña debe contener al menos 5 caracteres. <br>";
+      document.querySelector("#register-error").innerHTML +=
+        "La contraseña debe contener al menos 5 caracteres. <br>";
     }
     if (passwordUppercaseCount === 0) {
-      errorMessage +=
+      document.querySelector("#register-error").innerHTML +=
         "La contraseña debe contener al menos una mayúscula. <br>";
     }
     if (passwordLowercaseCount === 0) {
-      errorMessage +=
+      document.querySelector("#register-error").innerHTML +=
         "La contraseña debe contener al menos una minúscula. <br>";
     }
     if (passwordNumberCount === 0) {
-      errorMessage += "La contraseña debe contener al menos un número.";
+      document.querySelector("#register-error").innerHTML +=
+        "La contraseña debe contener al menos un número.";
     }
   } else {
-    errorMessage = "";
-    successMessage = "";
     passwordValidated = true;
   }
 
-  document.querySelector("#register-errorMessage").innerHTML = errorMessage;
-  document.querySelector("#register-success").innerHTML = successMessage;
   return passwordValidated;
 }
 
@@ -129,7 +127,7 @@ function validateCard(creditCard) {
     }
   }
 
-  for (let pos = creditCard.length - 2; pos >= 0; pos -= 2) { 
+  for (let pos = creditCard.length - 2; pos >= 0; pos -= 2) {
     addedUpNumber += Number(creditCard.charAt(pos));
   }
 
@@ -137,10 +135,12 @@ function validateCard(creditCard) {
     cardValidated = true;
   } else {
     cardValidated = false;
+    document.querySelector("#register-error").innerHTML +=
+    "La tarjeta ingresada es incorrecta. <br>"
   }
 
   return cardValidated;
-} 
+}
 
 function validateUsername(username) {
   let usernameValidated = true;
@@ -148,39 +148,122 @@ function validateUsername(username) {
   for (let i = 0; i < mainApp.userList.length; i++) {
     if (username === mainApp.userList[i].username) {
       usernameValidated = false;
+    } else {
+      document.querySelector("#register-error").innerHTML +=
+      "El nombre de usuario ya existe. <br>";
     }
-  } 
+  }
 
   return usernameValidated;
 }
 
+function validateCVC(cvc) {
+  let cvcValidated = false;
+  
+  for (let i = 0; i < cvc.length; i++) {
+    if (cvc.length === 3) {
+      cvcValidated = true;
+    } else {
+      document.querySelector("#register-error").innerHTML +=
+      "El CVC solamente puede tener 3 digitos"
+    }
+  }
+
+  return cvcValidated;
+}
+
 function preloadUsers() {
-  let preloadedUser = new User("Martin", "Leib", "mleib", "1234", "4111111111111111", "999", "root");
+  let preloadedUser = new User(
+    "Martin",
+    "Leib",
+    "mleib",
+    "1234",
+    "4111111111111111",
+    "999",
+    "root"
+  );
   mainApp.userList.push(preloadedUser);
-  preloadedUser = new User("Martin", "Etchebarne", "tinchoet", "1234", "4111111111111111", "999", "root");
+  preloadedUser = new User(
+    "Martin",
+    "Etchebarne",
+    "tinchoet",
+    "1234",
+    "4111111111111111",
+    "999",
+    "root"
+  );
   mainApp.userList.push(preloadedUser);
-  preloadedUser = new User("Juan", "Perez", "jperez", "1234", "4111111111111111", "999", "user");
+  preloadedUser = new User(
+    "Juan",
+    "Perez",
+    "jperez",
+    "1234",
+    "4111111111111111",
+    "999",
+    "user"
+  );
   mainApp.userList.push(preloadedUser);
-  preloadedUser = new User("Carlos", "Rodriguez", "crodriguez", "1234", "4111111111111111", "999", "user");
+  preloadedUser = new User(
+    "Carlos",
+    "Rodriguez",
+    "crodriguez",
+    "1234",
+    "4111111111111111",
+    "999",
+    "user"
+  );
   mainApp.userList.push(preloadedUser);
-  preloadedUser = new User("Marcos", "Gonzales", "mgonzales", "1234", "4111111111111111", "999", "user");
+  preloadedUser = new User(
+    "Marcos",
+    "Gonzales",
+    "mgonzales",
+    "1234",
+    "4111111111111111",
+    "999",
+    "user"
+  );
   mainApp.userList.push(preloadedUser);
 
-  console.log(`Cantidad de usuarios en la base de datos: ${mainApp.userList.length}.`);
+  console.log(
+    `Cantidad de usuarios en la base de datos: ${mainApp.userList.length}.`
+  );
   console.log(mainApp.userList);
 }
 
 function checkVoidInputs(input1, input2, input3, input4, input5, input6) {
   let voidInputs = false;
 
-  if (input1 === "" || input2 === "" || input3 === "" || input4 === "" || input5 === "" || input6 === "") {
+  if (
+    input1 === "" ||
+    input2 === "" ||
+    input3 === "" ||
+    input4 === "" ||
+    input5 === "" ||
+    input6 === ""
+  ) {
     voidInputs = true;
+    document.querySelector("#register-error").innerHTML += "No pueden haber campos vacios. <br>";
   }
 
   return voidInputs;
-}  
+}
 
-function createNewUser(firstName, lastName, password, username, creditCard, cvc) {
-  let newUser = new User (firstName, lastName, password, username, creditCard, cvc, "user");
+function createNewUser(
+  firstName,
+  lastName,
+  password,
+  username,
+  creditCard,
+  cvc
+) {
+  let newUser = new User(
+    firstName,
+    lastName,
+    password,
+    username,
+    creditCard,
+    cvc,
+    "user"
+  );
   mainApp.userList.push(newUser);
 }
