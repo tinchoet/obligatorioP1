114fafe1192e8c4f701ef260641194d62f83b320
VascoEt <arieletchebarne@outlook.com>
2024-06-10 00:32:58 -0300

    Filtro de Compras

--- js/app.js
+++ js/app.js
@@ -42,14 +42,13 @@ window.addEventListener("load", () => {
     .querySelector("#header-hidden-logout-button")
     .addEventListener("click", logout); // logout
 
-  // No se que tan buena idea sea llamar dos funciones con el mismo ID.
-  // ósea, funcionar funciona, pero no se si sea una buena práctica.
-  // Me da la impresión de que vamos a romper todo haciendo esto así  ---leib
+
   document.querySelector("#header-hidden-show-sales-button").addEventListener("click", showSales);
   document.querySelector("#header-hidden-show-sales-button").addEventListener("click", toggleSalesListDisplay);
   document.querySelector("#header-hidden-show-earnings-button").addEventListener("click", showEarnings);
   document.querySelector("#header-hidden-show-earnings-button").addEventListener("click", toggleEarningsDisplay);
 
+  document.querySelector("#sales-actions").addEventListener("change", showFilterSales);
   // Header: vista administrador, cambio de usuarios
   document.querySelector("#text-current-view").style.display = "none";
   document
@@ -117,7 +116,7 @@ function logout() {
   document.querySelector("#register-container").style.display = "none";
   document.querySelector("#text-current-view").style.display = "none";
   document.querySelector("#header-sales-actions").style.display = "none";
-  
+
   // Al hacer logout muestra la pestaña de Login y esconde el header (opciones de administrador)
   showLogin();
   hideHeaderHiddenActions();
@@ -713,9 +712,8 @@ function showEarnings() {
                     <td>${loadingItem.buyer.username}</td>
                     <td>${loadingItem.product.name}</td>
                     <td>${loadingItem.amountPurchased}</td>
-                    <td>${
-                      loadingItem.amountPurchased * loadingItem.product.price
-                    } USD</td>
+                    <td>${loadingItem.amountPurchased * loadingItem.product.price
+        } USD</td>
                 </tr>`;
     }
   }
@@ -748,3 +746,81 @@ function toggleEarningsDisplay() {
   document.querySelector("#products-list").style.display = "none";
   document.querySelector("#sales-list").style.display = "none";
 }
+
+
+function showFilterSales() {
+  let salesFilter = document.querySelector('#sales-actions').value;
+  let HTMLtable = "";
+
+  switch (salesFilter) {
+    case ('Aprobadas'):
+      let approved = mainApp.salesList.filter((Sale) => Sale.purchaseStatus === "Aprobada");
+      HTMLtable = "<table border='1' align='center'>";
+      HTMLtable += `<tr>
+                    <th>Nombre comprador</th>
+                    <th>Nombre producto</th>
+                    <th>Unidades compradas</th>
+                    <th>Total</th>
+                </tr>`;
+      for (let i = 0; i < approved.length; i++) {
+        let loadingItem = approved[i];
+        HTMLtable += `<tr>
+                      <td>${loadingItem.buyer.username}</td>
+                      <td>${loadingItem.product.name}</td>
+                      <td>${loadingItem.amountPurchased}</td>
+                      <td>${loadingItem.amountPurchased * loadingItem.product.price} USD</td>
+                  </tr>`;
+      }
+      HTMLtable += "</table>";
+      document.querySelector("#sales-list").innerHTML = HTMLtable;
+      break;
+
+    case ('Pendientes'):
+      let pending = mainApp.salesList.filter((Sale) => Sale.purchaseStatus === "Pendiente");
+      HTMLtable = "<table border='1' align='center'>";
+      HTMLtable += `<tr>
+                    <th>Nombre comprador</th>
+                    <th>Nombre producto</th>
+                    <th>Unidades compradas</th>
+                    <th>Total</th>
+                </tr>`;
+      for (let i = 0; i < pending.length; i++) {
+        let loadingItem = pending[i];
+        HTMLtable += `<tr>
+                      <td>${loadingItem.buyer.username}</td>
+                      <td>${loadingItem.product.name}</td>
+                      <td>${loadingItem.amountPurchased}</td>
+                      <td>${loadingItem.amountPurchased * loadingItem.product.price} USD</td>
+                  </tr>`;
+      }
+      HTMLtable += "</table>";
+      document.querySelector("#sales-list").innerHTML = HTMLtable;
+      break;
+
+    case ('Canceladas'):
+      let canceled = mainApp.salesList.filter((Sale) => Sale.purchaseStatus === "Cancelada");
+      HTMLtable = "<table border='1' align='center'>";
+      HTMLtable += `<tr>
+                    <th>Nombre comprador</th>
+                    <th>Nombre producto</th>
+                    <th>Unidades compradas</th>
+                    <th>Total</th>
+                </tr>`;
+      for (let i = 0; i < canceled.length; i++) {
+        let loadingItem = canceled[i];
+        HTMLtable += `<tr>
+                      <td>${loadingItem.buyer.username}</td>
+                      <td>${loadingItem.product.name}</td>
+                      <td>${loadingItem.amountPurchased}</td>
+                      <td>${loadingItem.amountPurchased * loadingItem.product.price} USD</td>
+                  </tr>`;
+      }
+      HTMLtable += "</table>";
+      document.querySelector("#sales-list").innerHTML = HTMLtable;
+      break;
+    default:
+      showSales();
+    }
+
+}
+
